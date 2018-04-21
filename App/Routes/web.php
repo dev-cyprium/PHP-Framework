61ee83a4e87ae07a2849cb561184c2d3a20da5d5
Stefan Kupresak <stefan_vg@hotmail.com>
2018-04-21 16:50:56 +0200

    Add docs to the router

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -1,6 +1,8 @@
 <?php
   /*
     All application routes are defined in this file.
+    $route object is a router object which can be used to
+    define application routes.
   */
 
   $route->get('/')->to('IndexController')->act('index');
