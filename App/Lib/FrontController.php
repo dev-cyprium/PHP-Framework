6366c6b212ef5efd21ceab16caaf91d5e911f909
Stefan Kupresak <stefan_vg@hotmail.com>
2018-04-21 05:27:55 +0200

    Add method implementations for the interface

--- App/Lib/FrontController.php
+++ App/Lib/FrontController.php
@@ -3,5 +3,23 @@
 
   class FrontController implements FrontControllerInterface
   {
+    public function setController($controller)
+    {
 
+    }
+
+    public function setAction($action)
+    {
+
+    }
+
+    public function setParams(array $params)
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
   }
